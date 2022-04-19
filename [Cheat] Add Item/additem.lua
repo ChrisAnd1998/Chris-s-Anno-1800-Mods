699522b135a0ff1cec4723b9ecdd8212ef251034
Chris Andriessen <50437199+ChrisAnd1998@users.noreply.github.com>
2022-04-19 18:14:51 +0200

    Add files via upload

--- [Cheat] Add Item/additem.lua
+++ [Cheat] Add Item/additem.lua
@@ -3,21 +3,26 @@ local GVal = ""
 
 local function getInput(inputval)
 	GVal = GVal .. inputval
-	print("getInput" .. GVal)
+	print("Recieved intput: " .. GVal)
 end
 
 local function confirmInput()
 	local num = tonumber(GVal)
-	print(num)
-	ts.Selection.Object.ItemContainer.SetCheatItemInSlot(num, 1)
+	TextSources.TextSourceRoots.Selection.Object.ItemContainer.SetEquipSlot(0, 0)
+	TextSources.TextSourceRoots.Selection.Object.ItemContainer.SetClearSlot(0)
+	TextSources.TextSourceRoots.Selection.Object.ItemContainer.SetCheatItemInSlot(num, 1)
+	print("Input " .. num .. " confirmed!")
+	print("Sending command: TextSources.TextSourceRoots.Selection.Object.ItemContainer.SetCheatItemInSlot(" .. num .. ", 1)")
 	GVal = ""
-	print("confirmInput")
+	num = ""
+	print("Input has been reset!")
 end
 
 local function resetInput()
 	GVal = ""
-	print("resetInput")
+	print("Input has been reset!")
 end
+
 
 addItem = {
 	resetInput = resetInput,
